7ca38c64671abd2ad4b4794f210296860362b950
jaishaan <jlalwani0001@mymail.lausd.net>
2026-05-29 11:19:59 -0700

    dance

--- src/main/java/com/elprojecto/TesterFinch.java
+++ src/main/java/com/elprojecto/TesterFinch.java
@@ -3,7 +3,17 @@ package com.elprojecto;
 public class TesterFinch {
     public static void main(String[] args) {
         FinalFinch bird = new FinalFinch("A");
-        bird.setMove("F", 10, 50);
+
+
+
+for (int i = 0; i < 2; i++) {
+bird.setMove("F", 50, 50);
+bird.setBeak(100, 100, 100);
+bird.setTurn("R", 360.0, 40);
+bird.setMove("B", 50, 50);
+bird.setTurn("L", 360.0, 40);
+}
+
     }
 
 
